890a20622cac1b1b79ead26a561fb0276a7c010d
Valentin Destruel <destruel.valentin@gmail.com>
2018-03-13 10:37:50 +0100

    DatePickerDialog

--- app/src/main/java/fr/wcs/blablawild/ItinerarySearchActivity.java
+++ app/src/main/java/fr/wcs/blablawild/ItinerarySearchActivity.java
@@ -1,12 +1,18 @@
 package fr.wcs.blablawild;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class ItinerarySearchActivity extends AppCompatActivity {
 
@@ -14,6 +20,41 @@ public class ItinerarySearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_itinerary_search);
+
+        final Calendar myCalendar = Calendar.getInstance();
+        final EditText editDate= (EditText) findViewById(R.id.edit_date);
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            private void updateLabel() {
+                String myFormat = "MM/dd/yy";
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.FRANCE);
+
+                editDate.setText(sdf.format(myCalendar.getTime()));
+            }
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+
+        editDate.setOnClickListener(new View.OnClickListener()
+
+        {
+
+            @Override
+            public void onClick (View v){
+
+                new DatePickerDialog(ItinerarySearchActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
         final Button buttonSearch = findViewById(R.id.button_search);
         buttonSearch.setOnClickListener(new View.OnClickListener() {
